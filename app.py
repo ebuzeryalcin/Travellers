1e412d0c20a415e87a8d50073c85c883dcabcbab
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-03-07 21:48:33 +0000

    Created register.html and base.html, will make changes in my next commit.

--- app.py
+++ app.py
@@ -22,6 +22,11 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/register/", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
