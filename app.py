90ca93345b955898b32e6186cee737d3b87d50d1
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-03-09 23:24:40 +0000

    Added post method in app.py so data have an action.connected user registration to mongodb.

--- app.py
+++ app.py
@@ -25,6 +25,25 @@ def home():
 
 @app.route("/register/", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # User check in database if existing
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into session/temporary cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+
     return render_template("register.html")
 
 
