7f35662f2bcae3e250c251fa40accd1f39535eca
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-03-29 20:42:04 +0000

    Added onscroll function to my place cards. Added neccessary script.

---
+++ static/js/script.js
@@ -0,0 +1,4 @@
+// Used onscroll function from this link https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_onscroll
+function scrollcard() {
+  document.getElementById("scrolling-card");
+}
