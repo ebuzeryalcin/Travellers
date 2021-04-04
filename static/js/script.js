fc2d518a34072be4060f7983e59343878acc964e
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-04-04 21:56:32 +0000

    Added content to footer in base.html.

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@
 function scrollcard() {
   document.getElementById("scrolling-card");
 }
-
+// Used function from this link https://stackoverflow.com/questions/36719982/error-message-when-a-user-enters-more-characters-than-max-length-on-text-area
 $("#place_description").keyup(function(){
       $("#desc-char").text("Characters left: " + (250 - $(this).val().length));
     });
