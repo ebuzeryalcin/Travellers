ec2bfdd5a26c7cce1f9c555073179de29df91d53
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-04-12 09:39:55 +0000

    Turned debug to False in app.py.

--- app.py
+++ app.py
@@ -187,4 +187,4 @@ def delete_place(place_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
